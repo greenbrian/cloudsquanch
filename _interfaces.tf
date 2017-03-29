91193f61fe7b43d289c4216f09d2dde39f1eab50
Brian Green <greenbrian@gmail.com>
2017-03-29 09:05:22 -0500

    interfaces confusion

--- _interfaces.tf
+++ _interfaces.tf
@@ -2,7 +2,9 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
+variable "admin_username" {}
+variable "admin_password" {}
 
-output "azure_ubuntu_demo_rg" {
-  value = "${azure_rm_resource_group.cloudsquanch.name}"
+output "Cloudsquanch Public IP" {
+  value = "${azurerm_public_ip.cloudsquanch.ip_address}"
 }
