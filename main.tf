990e013abe8693393dd03234de8b3a310cc57b40
Brian Green <greenbrian@gmail.com>
2017-03-28 23:43:52 -0500

    trying yet again

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_virtual_machine" "cloudsquanch" {
   storage_os_disk {
     name          = "myosdisk1"
     vhd_uri       = "${azurerm_storage_account.cloudsquanch.primary_blob_endpoint}${azurerm_storage_container.cloudsquanch.name}/myosdisk1.vhd"
-    image_uri     = "${data.atlas_artifact.custom_image.azure-arm.Azure.ResourceManagement.VMImage.OSDiskUri}"
+    image_uri     = "${data.atlas_artifact.custom_image.OSDiskUri}"
     os_type       = "linux"
     caching       = "ReadWrite"
     create_option = "FromImage"
