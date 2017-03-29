d8101fbe65375f590f09e25ae328dff4cc52dae6
Brian Green <greenbrian@gmail.com>
2017-03-28 20:20:46 -0500

    updated with arm variables

--- _interfaces.tf
+++ _interfaces.tf
@@ -1,3 +1,7 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
 variable "admin_username" {}
 variable "admin_password" {}
 variable "storage_account_name" {}
