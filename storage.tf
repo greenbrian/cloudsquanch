29b95af9f76edb9e3a432e0e56461e39998a391a
Brian Green <greenbrian@gmail.com>
2017-03-29 09:15:13 -0500

    updated resource group reference for remote state

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "cloudsquanch" {
   name                = "${random_id.storage_account.keepers.environment_name}${random_id.storage_account.hex}"
-  resource_group_name = "${azurerm_resource_group.cloudsquanch.name}"
+  resource_group_name = "${data.terraform_remote_state.azure_ubuntu_demo_rg.azure_ubuntu_demo_rg}"
   location            = "centralus"
   account_type        = "Standard_LRS"
 
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "cloudsquanch" {
 
 resource "azurerm_storage_container" "cloudsquanch" {
   name                  = "vhds"
-  resource_group_name   = "${azurerm_resource_group.cloudsquanch.name}"
+  resource_group_name   = "${data.terraform_remote_state.azure_ubuntu_demo_rg.azure_ubuntu_demo_rg}"
   storage_account_name  = "${azurerm_storage_account.cloudsquanch.name}"
   container_access_type = "private"
 }
